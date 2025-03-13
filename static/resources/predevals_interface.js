47bf282213b07c16febfdc8ae87579a666ae598e
Evan Ray <elray1@users.noreply.github.com>
2025-03-13 10:01:41 -0400

    bump predevals version to 1.0.0

--- static/resources/predevals_interface.js
+++ static/resources/predevals_interface.js
@@ -1,4 +1,4 @@
-import App from "https://cdn.jsdelivr.net/gh/hubverse-org/predevals@0.1.1/dist/predevals.bundle.js";
+import App from "https://cdn.jsdelivr.net/gh/hubverse-org/predevals@1.0.0/dist/predevals.bundle.js";
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
 document.predevals = App;  // for debugging
